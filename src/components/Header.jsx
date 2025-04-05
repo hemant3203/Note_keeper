4195a8796e211fee438e43242844512651856574
hemant3203 <prajapatihero341@gmail.com>
2025-04-05 15:50:21 +0530

    note Keeper using React JS

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import LightbulbIcon from '@mui/icons-material/Lightbulb';
+import EmojiObjectsIcon from '@mui/icons-material/EmojiObjects';
+
+function Header() {
+  return (
+    <header>
+     <h1> < EmojiObjectsIcon/>Keeper</h1>
+    </header>
+  );
+}
+
+export default Header;
